7cb052365b5e4eb2583d2be0ddfbe559e5bd8b79
fenceFoil <karnavasbj@gmail.com>
2019-11-21 18:59:15 -0500

    Specify aws region

--- doorscript/deployquipgen.py
+++ doorscript/deployquipgen.py
@@ -5,7 +5,7 @@ import datetime
 
 startTime = datetime.datetime.now()
 
-ec2 = boto3.resource('ec2')
+ec2 = boto3.Session(region_name="us-west-2").resource('ec2')
 userData = open('awsuserdata.txt',mode='r').read()
 instances = ec2.create_instances(ImageId='ami-04121e1f9d541d468', InstanceType='g4dn.xlarge', MaxCount=1, MinCount=1, InstanceInitiatedShutdownBehavior='terminate', KeyName='quipgenkey', SecurityGroupIds=['quipgen'], UserData=userData)
 quipgenServerIP = None
@@ -26,6 +26,6 @@ while response == None or response.status_code != 200:
         print ("Timeout...")
     except requests.exceptions.ConnectionError:
         print ("Connection refused...")
-        
+
 print ("Quipgen responded! Time to launch: {} seconds".format((datetime.datetime.now()-startTime).seconds))
 print (quipgenServerIP)
