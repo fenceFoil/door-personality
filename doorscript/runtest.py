b4121027674074c874252471dfd2fbe0d15a01ee
fenceFoil <karnavasbj@gmail.com>
2019-11-21 17:32:45 -0500

    Read out quotes from a queue

--- doorscript/runtest.py
+++ doorscript/runtest.py
@@ -1,3 +1,10 @@
 import os
+import random
 
-os.system('cat text.txt | espeak --stdin -a30 -g1 -p30 -m')
+def getQuoteFiles():
+    return [f for f in os.listdir(dir) if f.endswith('.txt')]
+
+quoteFile = random.choice(getQuoteFiles())
+os.system('cat ~/door-personality/queuedQuotes/{} | espeak --stdin -a30 -g1 -p30 -m'.format(quoteFile))
+os.remove(quoteFile)
+print ("Quotes remaining: {}".format(len(getQuoteFiles())))
