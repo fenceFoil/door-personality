e18cfd777a8b42b5b9433131d00b3cccf9cc0a73
fenceFoil <karnavasbj@gmail.com>
2019-11-21 16:14:44 -0500

    Successfully starting GPT2 demo server now

--- doorscript/startquipgen.py
+++ doorscript/startquipgen.py
@@ -4,6 +4,9 @@ import time
 ec2 = boto3.resource('ec2')
 userData = open('awsuserdata.txt',mode='r').read()
 instances = ec2.create_instances(ImageId='ami-04121e1f9d541d468', InstanceType='g4dn.xlarge', MaxCount=1, MinCount=1, InstanceInitiatedShutdownBehavior='terminate', KeyName='quipgenkey', SecurityGroupIds=['quipgen'], UserData=userData)
-time.sleep(5)
-instances[0].reload()
-print(instances[0].public_ip_address)
+quipgenServerIP = None
+while quipgenServerIP == None:
+    time.sleep(1)
+    instances[0].reload()
+    quipgenServerIP = instances[0].public_ip_address
+print (quipgenServerIP)
