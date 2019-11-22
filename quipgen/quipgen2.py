b903fa21a3ac77b6fd7f2cc3ecabc8f57bf40f71
fenceFoil <karnavasbj@gmail.com>
2019-11-22 00:46:44 -0500

    a

--- quipgen/quipgen2.py
+++ quipgen/quipgen2.py
@@ -10,6 +10,6 @@ app = Flask(__name__)
 @app.route('/gpt2')
 def gpt2():
     prompt = "It's a beautiful day, and the door said, "
-    print ("FUNCTION OUTPUT: {}".format(subprocess.call('python run_generation.py --model_type gpt2 --model_name_or_path gpt2-medium --prompt "{}"'.format(prompt), shell=True)))
-    with open('run_generation_output.pkl', 'r') as f:
+    subprocess.call('python run_generation.py --model_type gpt2 --model_name_or_path gpt2-medium --prompt "{}"'.format(prompt), shell=True)
+    with open('run_generation_output.pkl', 'b') as f:
         return jsonify(pickle.load(f))
