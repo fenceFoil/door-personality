7d94e0db239ed7ea49b075bd646b92b101dc7532
fenceFoil <karnavasbj@gmail.com>
2019-11-22 01:38:29 -0500

    seed is now random

--- quipgen/quipgen2.py
+++ quipgen/quipgen2.py
@@ -1,5 +1,6 @@
 import pickle
 import subprocess
+import random
 
 from flask import Flask, jsonify, request
 
@@ -14,15 +15,15 @@ def sortSentencesBySentiment(sentences):
     return sorted(sentences, key=lambda sentence: sentimentAnalyzer.polarity_scores(sentence)['compound'])
 
 import nltk, nltk.data
+nltk.download('punkt')
 def splitBySentences(text):
-    nltk.download('punkt')
     tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
     return tokenizer.tokenize(text)
 
 @app.route('/gpt2')
 def gpt2():
     prompt = "It's a beautiful day, and the door said, "
-    subprocess.call('python run_generation.py --model_type gpt2 --num_samples 10 --model_name_or_path distilgpt2 --prompt "{}"'.format(prompt), shell=True)
+    subprocess.call('python run_generation.py --seed {} --model_type gpt2 --num_samples 10 --model_name_or_path distilgpt2 --prompt "{}"'.format(random.randint(0, 100000000), prompt), shell=True)
     with open('run_generation_output.pkl', 'rb') as f:
         generatedSamples = pickle.load(f)
     generatedSentences = [splitBySentences(s)[0] for s in generatedSamples]
