bf79ecc2686e66fec12c77b669fcc5400c1f7f6a
fenceFoil <karnavasbj@gmail.com>
2019-11-22 00:23:09 -0500

    Isolating gpt2 sentence gen

--- quipgen/quipgen2.py
+++ quipgen/quipgen2.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle, os
 
 from flask import Flask, jsonify, request
 
@@ -8,5 +8,7 @@ app = Flask(__name__)
 
 @app.route('/gpt2')
 def gpt2():
+    prompt = "It's a beautiful day, and the door said, "
+    os.system('python run_generation.py --model_type gpt2 --model_name_or_path gpt2-medium --prompt "{}"'.format(prompt))
     with open('run_generation_output.pkl', 'r') as f:
         return jsonify(pickle.load(f))
