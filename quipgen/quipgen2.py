d29cb2da2b87083009ec2bd2f304653ea86dad93
fenceFoil <karnavasbj@gmail.com>
2019-11-22 01:42:44 -0500

    Take top cheeriest of a number of sentences

--- quipgen/quipgen2.py
+++ quipgen/quipgen2.py
@@ -12,7 +12,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 sentimentAnalyzer = SentimentIntensityAnalyzer()
 def sortSentencesBySentiment(sentences):
     global sentimentAnalyzer
-    return sorted(sentences, key=lambda sentence: sentimentAnalyzer.polarity_scores(sentence)['compound'])
+    return sorted(sentences, reverse=True, key=lambda sentence: sentimentAnalyzer.polarity_scores(sentence)['compound'])
 
 import nltk, nltk.data
 nltk.download('punkt')
@@ -22,10 +22,15 @@ def splitBySentences(text):
 
 @app.route('/gpt2')
 def gpt2():
+    NUM_TEXT_SAMPLES = 10
+    NUM_QUIPS_RETURNED = 4
+
     prompt = "It's a beautiful day, and the door said, "
-    subprocess.call('python run_generation.py --seed {} --model_type gpt2 --num_samples 10 --model_name_or_path distilgpt2 --prompt "{}"'.format(random.randint(0, 100000000), prompt), shell=True)
+    subprocess.call('python run_generation.py --seed {} --model_type gpt2 --num_samples {} --model_name_or_path distilgpt2 --prompt "{}"'.format(random.randint(0, 100000000), NUM_TEXT_SAMPLES, prompt), shell=True)
     with open('run_generation_output.pkl', 'rb') as f:
         generatedSamples = pickle.load(f)
     generatedSentences = [splitBySentences(s)[0] for s in generatedSamples]
+    sortedSentences = sortSentencesBySentiment(generatedSentences)
+    topSentences = sortedSentences[0:NUM_QUIPS_RETURNED]
     return jsonify(sortSentencesBySentiment(generatedSentences))
     
