c36399c01643b9c775f2e7d6e5167f3790735167
fenceFoil <karnavasbj@gmail.com>
2019-11-22 00:30:20 -0500

    asdf

--- quipgen/quipgen2.py
+++ quipgen/quipgen2.py
@@ -1,4 +1,5 @@
-import pickle, os
+import pickle
+import subprocess
 
 from flask import Flask, jsonify, request
 
@@ -9,6 +10,6 @@ app = Flask(__name__)
 @app.route('/gpt2')
 def gpt2():
     prompt = "It's a beautiful day, and the door said, "
-    os.system('python run_generation.py --model_type gpt2 --model_name_or_path gpt2-medium --prompt "{}"'.format(prompt))
+    print ("FUNCTION OUTPUT: {}".format(subprocess.call('python run_generation.py --model_type gpt2 --model_name_or_path gpt2-medium --prompt "{}"'.format(prompt))))
     with open('run_generation_output.pkl', 'r') as f:
         return jsonify(pickle.load(f))
