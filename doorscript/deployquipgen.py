7f7dae226d81d3946e8ea21533747ed74dbb7a0b
fenceFoil <karnavasbj@gmail.com>
2020-01-22 01:59:34 -0500

    Added noquips mode

--- doorscript/deployquipgen.py
+++ doorscript/deployquipgen.py
@@ -93,12 +93,15 @@ SPOKEN_TEXT_DIR = DATA_DIR+'spokenQuipTexts/'
 UNSPOKEN_QUIPS_DIR = DATA_DIR+'unspokenQuips/'
 SPOKEN_QUIPS_DIR = DATA_DIR+'spokenQuips/'
 
-for i in range (10):
-    quipText = generateQuipText(quipgenServerIP)
-    quipID = str(uuid.uuid4())
-    with open(UNSPOKEN_TEXT_DIR+quipID, 'w+') as quipOut:
-        quipOut.write(quipText)
-    print ("Generated quip #{}".format(i+1))
+if len(sys.argv) >= 3 and sys.argv[2] == "noquips":
+    print ("Skipping quip generation...")
+else:
+    for i in range (10):
+        quipText = generateQuipText(quipgenServerIP)
+        quipID = str(uuid.uuid4())
+        with open(UNSPOKEN_TEXT_DIR+quipID, 'w+') as quipOut:
+            quipOut.write(quipText)
+        print ("Generated quip #{}".format(i+1))
 
 ### Convert text to quips
 
